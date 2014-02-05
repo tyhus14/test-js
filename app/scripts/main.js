6431f2f3df738e10aa85c3b699dbc46ceb529349
tyhus14 <huston.tyler3@gmail.com>
2014-02-05 16:16:24 -0500

    updated file and correct some test errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
 console.log('\'Allo \'Allo!');
+ 
+function Collection (models) {
+  this.models = models || [];
+ 
+  this.find = function(id) {
+    var result;
+ 
+    if (typeof(id) != 'string') {
+      throw new Error("Whoops!");
+    }
+ 
+    this.models.forEach(function(value, index){
+      if (value.id == id) {
+        result = value;
+      }
+    });
+ 
+    if (result) {
+      return result;
+    }
+  }
+}
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,14 +1,80 @@
 /* global describe, it */
-
+ 
 (function () {
     'use strict';
-
-    describe('Checking if object exists', function () {
-        describe('within the array models', function () {
-            it('should have two objects', function () {
-              var models = []
-              expect(models).to.have.length(2);
+ 
+    describe('A Collection constructor', function () {
+        describe(', when run', function () {
+            it('should return a new object', function () {
+              var students = new Collection;
+ 
+              expect(students.constructor.name).to.equal("Collection");
             });
+ 
+            it('stores its first param in a property called models', function(){
+              var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id:'2'}]
+              var students = new Collection(toBeAdded)
+              
+              expect(students.models).to.be.a('Array');
+              expect(students.models[1].name).to.equal('Jack');
+            });
+ 
         });
     });
+ 
+    describe("A Collection instance", function(){
+      describe("has a .find() method",function(){
+        it("should return an object when given an id that is present in the models", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('99')).to.deep.equal({name: 'Jim', id: '99'});
+        });
+ 
+        it("should not return an object when that id is not present in the models", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('1')).to.not.equal({name: 'Jim', id: '99'});
+        });
+ 
+        it("should return undefined when that id is not present in the models", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(students.find('1')).to.equal(undefined);
+        });
+ 
+        it("should throw an error when given an arguemnt other than a string", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+          expect(function(){students.find(1)}).to.throw(Error);
+          expect(function(){students.find({})}).to.throw(Error);
+          expect(function(){students.find([])}).to.throw(Error);
+        });
+
+        it('should return an object when given an name that is present in the models', function(){
+        	var students = new Collection([{name: 'Jim', id: '99'}]);
+        	expect(students.find('99')).to.deep.equal({name: 'Jim', id: '99'});
+        });
+
+        it('should return an object when given a name and id that is present in the models', function(){
+        	var students = new Collection([{name: 'Jim', id: '99'}]);
+        	expect(students.find('99')).to.deep.equal({name: 'Jim', id: '99'});
+        });
+
+      });
+ 
+      describe("has an .add() method",function(){
+        it("should add the object it's given to the models property");
+        it("should increase the models property's length by 1");
+        it("should only accept a single object as an argument");
+        it("should not  accept an empty object as an argument");
+        it("should throw an error when given an object without and id property");
+      });
+ 
+      describe("has a .remove() method",function(){
+        it("should, when given an id, remove the corresponding object from the models property")
+        it("should decrease the models property's length by 1");
+        it("should only accept a single string as an id argument");
+        it("should return true on successful removal");
+      })
+    })
 })();
+    
+
+
+
